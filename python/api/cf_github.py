ed216b281290e24083a179fc7b09ae0e6ad7cd7a
sanbo <sanbo.xyz@gmail.com>
2021-01-11 19:06:18 +0800

    update

--- python/api/cf_github.py
+++ python/api/cf_github.py
@@ -14,31 +14,34 @@ path = ""
 
 # 上传文件
 def update_filef(filename=''):
-    url = "https://api.github.com/repos/{}/{}/contents/{}{}".format(user, repo, path, filename)
-    # file_data=open_file(filename)
-    headers = {"Authorization": "token " + token}
-    content = B64s.file_to_base64(filename)
-    # print(content)
-    data = {
-        # "committer": {
-        #     "name": "sanbo",
-        #     "email": "sanbo.xyz@gmail.com"
-        # },
-        "message": "Upload by xyz",
-        "content": content
-    }
-    data = json.dumps(data)
-    req = requests.put(url=url, data=data, headers=headers)
-    req.encoding = "utf-8"
-    re_data = json.loads(req.text)
-    # print("re_data: {}".format(re_data))
-    down_url = re_data['content']['download_url']
-    sha = re_data['content']['sha']
-    print("\tupload file sha: " + sha)
-    # 在国内默认的down_url可能会无法访问，因此使用CDN访问
-    cdn_url = "https://cdn.jsdelivr.net/gh/{}/{}/{}{}".format(user, repo, path, urllib.parse.quote(filename))
-    print("\tChina down path: {}".format(cdn_url))
-    print("\tGithub download_url: " + down_url)
+    try:
+        url = "https://api.github.com/repos/{}/{}/contents/{}{}".format(user, repo, path, filename)
+        # file_data=open_file(filename)
+        headers = {"Authorization": "token " + token}
+        content = B64s.file_to_base64(filename)
+        # print(content)
+        data = {
+            # "committer": {
+            #     "name": "sanbo",
+            #     "email": "sanbo.xyz@gmail.com"
+            # },
+            "message": "Upload by xyz",
+            "content": content
+        }
+        data = json.dumps(data)
+        req = requests.put(url=url, data=data, headers=headers)
+        req.encoding = "utf-8"
+        re_data = json.loads(req.text)
+        # print("re_data: {}".format(re_data))
+        down_url = re_data['content']['download_url']
+        sha = re_data['content']['sha']
+        print("\tupload file sha: " + sha)
+        # 在国内默认的down_url可能会无法访问，因此使用CDN访问
+        cdn_url = "https://cdn.jsdelivr.net/gh/{}/{}/{}{}".format(user, repo, path, urllib.parse.quote(filename))
+        print("\tChina down path: {}".format(cdn_url))
+        print("\tGithub download_url: " + down_url)
+    except:
+        pass
     try:
         os.remove(filename)
     except:
